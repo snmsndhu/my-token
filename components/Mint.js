8c4dc5e8d79e88f6b74a95a9bb250bb73055d182
Sandeep Singh <sandeepsingh@Sandeeps-MacBook-Air.local>
2022-10-26 12:58:10 -0400

    initial commit.

---
+++ components/Mint.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Mint() {
+  return (
+    <div>
+      <h1>S47</h1>
+      <input type="number" />
+      <button>Mint</button>
+    </div>
+  );
+}
+
+export default Mint;
---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <h3>S47</h3>
+      </div>
+      <div>
+        <button>Connect Wallet</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
